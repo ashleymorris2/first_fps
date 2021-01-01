4c5c48d93c87ec6f89877101b4c7f32473f135c2
ashley.morris <ashleym9000@googlemail.com>
2021-01-01 23:34:24 +0000

    Fixed bullt physics and particle effects

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,40 +3,76 @@
 public class BulletController : MonoBehaviour
 {
 
-    [SerializeField] float movementVelocity;
+    [SerializeField] float meshSkinWidth = 0.1f;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] float bulletVelocity;
     [SerializeField] float lifeTime;
-
     [SerializeField] GameObject collisionEffect;
-
     [SerializeField] Transform bulletDirection;
     
-    private Rigidbody bulletBody;
-    private GameObject parentObject;
+    private float minimumMoveExtent;
+    private float partialMoveExtent;
+    private float sqrMinimumMoveExtent;
+    private Vector3 previousPosition;
+
+    private GameObject bullet;
+    private Rigidbody bulletModel;
+    private Collider bulletCollider;
 
 
     void Start()
     {
-        bulletBody = GetComponent<Rigidbody>();
-        parentObject = transform.parent.gameObject;
+        bullet = transform.parent.gameObject;
+        bulletModel = GetComponent<Rigidbody>();
+        bulletCollider = bulletModel.GetComponent<Collider>();
+        
+        previousPosition = bulletModel.position;
+        
+        minimumMoveExtent = Mathf.Min(Mathf.Min(bulletCollider.bounds.extents.x, bulletCollider.bounds.extents.y), bulletCollider.bounds.extents.z);
+        partialMoveExtent = minimumMoveExtent * (1.0f  - meshSkinWidth);
+        sqrMinimumMoveExtent = minimumMoveExtent * partialMoveExtent;
     }
-
 
     void FixedUpdate()
     {
-        bulletBody.AddForce(bulletDirection.forward * movementVelocity, ForceMode.VelocityChange);
+        Vector3 movementThisUpdate = bulletModel.position - previousPosition;
+        float movementSqrMagnitude = movementThisUpdate.sqrMagnitude;
+
+        if(movementSqrMagnitude > sqrMinimumMoveExtent)
+        {
+            var movementMagnitude = Mathf.Sqrt(movementSqrMagnitude);
+
+            if(Physics.Raycast(previousPosition, movementThisUpdate, out var hitInfo, movementMagnitude, ~layerMask))
+            {
+                if(!hitInfo.collider)
+                    return;
+
+                if (hitInfo.collider.isTrigger)
+                    hitInfo.collider.SendMessage("OnTriggerEnter", bulletCollider);
+
+                if (!hitInfo.collider.isTrigger)
+                    bulletModel.position = hitInfo.point - (movementThisUpdate / movementMagnitude) * partialMoveExtent;
+            }
+        }
+
+        // previousPosition = bulletModel.position;
+
+        bulletModel.AddForce(bulletDirection.forward * bulletVelocity, ForceMode.VelocityChange);
+        bullet.transform.position = bulletModel.position;
+        
         lifeTime -= Time.deltaTime;
 
         if (lifeTime <= 0)
         {
-            Destroy(parentObject);
+            Destroy(bullet);
             Debug.Log("Expired");
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Instantiate(collisionEffect, transform.position, transform.rotation);
-        Destroy(parentObject);
-        Debug.Log("Destroyed");
+        Instantiate(collisionEffect, bullet.transform.position + (bullet.transform.forward * ((-bulletVelocity * .25f) * Time.deltaTime)), bullet.transform.rotation);
+        Destroy(bullet);
+        Debug.Log($"Destroyed ----- I {gameObject} Hit {other}");
     }
 }
